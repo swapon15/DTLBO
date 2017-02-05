5f81cd20ecdb3b5124fc8157a00d889b9aff57f9
swapon15 <baricsedu@gmail.com>
2017-02-05 18:48:18 -0500

    basic stuffs

--- ExpResults/dtlbo/ExpResults/scripts/subjective.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-for i in *.out.stat
-do
- cut -f3 -d ' ' $i > ./$i.bstonly
-done
-
-paste ./*.bstonly >result.all
--- ExpResults/dtlbo/ExpResults/scripts/test_individuals.R
+++
@@ -1,9 +0,0 @@
-   file_test    = "test.out"
-   file_result  = file("temp_result",open="w");
-   con          = file(file_test, open="r")
-   line         = readLines(con)
-   for (i in 1:length(line)) {
-      if (i%%3 == 0)
-         writeLines(line[i], file_result)
-   }
-   close(file_result)    
